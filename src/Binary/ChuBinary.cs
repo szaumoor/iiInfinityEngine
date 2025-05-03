227ca1ba8ed71c58a0a3d735ba041385911c21bc
btigi <btigi@users.noreply.github.com>
2025-05-03 17:36:47 +0100

    CHU binary work

---
+++ src/Binary/ChuBinary.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ii.InfinityEngine.Binary
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct ChuHeaderBinary
+    {
+        public array4 ftype;
+        public array4 fversion;
+        public Int32 WindowCount;
+        public Int32 ControlOffset;
+        public Int32 WindowOffset;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct ChuWindow
+    {
+        public Int16 WindowId;
+        public Int16 Unknown02;
+        public Int16 XCoordinate;
+        public Int16 YCoordinate;
+        public Int16 Width;
+        public Int16 Height;
+        public Int16 BackgroundFlag;
+        public Int16 ControlCount;
+        public array8 BackgroundMos;
+        public Int16 FirstControlIndex;
+        public Int16 Unknown1a;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct ChuControlTable
+    {
+        public Int32 ControlsOffset;
+        public Int32 Length;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct ChuControlCommon
+    {
+        public Int32 ControlId;
+        public Int32 XCoordinate;
+        public Int32 YCoordinate;
+        public Int16 Width;
+        public Int16 Height;
+        public byte Type;
+        public byte Unknownd;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct ChuControlButton
+    {
+        public array8 ButtonImageBam;
+        public byte AnimationCycle;
+        public byte JustificationFlags;
+        public byte FrameIndexUnpressed;
+        public byte AnchorCoordinateX1;
+        public byte FrameIndexPressed;
+        public byte AnchorCoordinateX2;
+        public byte FrameIndexSelected;
+        public byte AnchorCoordinateY1;
+        public byte FrameIndexDisabled;
+        public byte AnchorCoordinateY2;
+    }
+
+    // Slider
+    // TextEdit
+    // TextArea
+    // Label
+    // Scrollbar
+}
